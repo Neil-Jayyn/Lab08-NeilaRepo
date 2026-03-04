54e82a4edf086ab4dfbf23da94c715432678c120
User <neilakhu87@gmail.com>
2026-03-03 17:58:08 -0700

    failing test for demo

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,5 +3,14 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+    @ Test
+    public void testHasCity(){
+        CustomList customList = new CustomList();
+        City calgary = new City("Calgary","AB");
+        customList.addCity(calgary);
+
+        //should fail since theres no hasCity()
+        assertTrue(customList.hasCity(calgary));
+    }
 
 }
